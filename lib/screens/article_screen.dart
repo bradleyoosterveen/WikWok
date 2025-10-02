23856d7d54ae1a4fd62ce51684723b2506004160
bradleyoosterveen <34512356+bradleyoosterveen@users.noreply.github.com>
2025-10-02 20:35:32 +0200

    Fix save article button not showing (#16)
    * Wrap article content in StatefulBuilder
    
    * Update pubspec.yaml

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -69,104 +69,106 @@ class _ViewState extends State<_View> {
             ));
   }
 
-  Widget _content(
-    Article article,
-  ) {
-    return Column(
-      children: [
-        Expanded(
-          child: WikWokBanner(
-            src: article.image,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(24),
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+  Widget _content(Article article) => StatefulBuilder(builder: (context, _) {
+        context.read<SaveArticleCubit>().get(article.title);
+
+        return Column(
+          children: [
+            Expanded(
+              child: WikWokBanner(
+                src: article.image,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: SafeArea(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            article.subtitle,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelMedium
-                                ?.apply(
-                                  color: Colors.white.withValues(alpha: 0.76),
-                                ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Text(
-                            article.title,
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
-                          const SizedBox(height: 12),
-                          Text(
-                            article.content,
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            maxLines: 6,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 12),
-                    Column(
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        BlocBuilder<SaveArticleCubit, bool?>(
-                          builder: (context, state) => switch (state) {
-                            true => WikWokIconButton(
-                                icon: Icons.bookmark,
-                                label: 'Saved',
-                                onPressed: () => context
-                                    .read<SaveArticleCubit>()
-                                    .unsave(article.title),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                article.subtitle,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelMedium
+                                    ?.apply(
+                                      color:
+                                          Colors.white.withValues(alpha: 0.76),
+                                    ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
                               ),
-                            false => WikWokIconButton(
-                                icon: Icons.bookmark_outline,
-                                label: 'Save',
-                                onPressed: () => context
-                                    .read<SaveArticleCubit>()
-                                    .save(article.title),
+                              Text(
+                                article.title,
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
                               ),
-                            _ => const SizedBox.shrink(),
-                          },
+                              const SizedBox(height: 12),
+                              Text(
+                                article.content,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                maxLines: 6,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
                         ),
-                        WikWokIconButton(
-                          icon: Icons.share,
-                          label: 'Share',
-                          onPressed: () {
-                            context
-                                .read<ArticleCubit>()
-                                .copyToClipboard(context, article.title);
-                          },
-                        ),
-                        WikWokIconButton(
-                          icon: Icons.open_in_new,
-                          label: 'Open',
-                          onPressed: () {
-                            context
-                                .read<ArticleCubit>()
-                                .openUrl(context, article.title);
-                          },
+                        const SizedBox(width: 12),
+                        Column(
+                          children: [
+                            BlocBuilder<SaveArticleCubit, bool?>(
+                              builder: (context, state) => switch (state) {
+                                true => WikWokIconButton(
+                                    icon: Icons.bookmark,
+                                    label: 'Saved',
+                                    onPressed: () => context
+                                        .read<SaveArticleCubit>()
+                                        .unsave(article.title),
+                                  ),
+                                false => WikWokIconButton(
+                                    icon: Icons.bookmark_outline,
+                                    label: 'Save',
+                                    onPressed: () => context
+                                        .read<SaveArticleCubit>()
+                                        .save(article.title),
+                                  ),
+                                _ => const SizedBox.shrink(),
+                              },
+                            ),
+                            WikWokIconButton(
+                              icon: Icons.share,
+                              label: 'Share',
+                              onPressed: () {
+                                context
+                                    .read<ArticleCubit>()
+                                    .copyToClipboard(context, article.title);
+                              },
+                            ),
+                            WikWokIconButton(
+                              icon: Icons.open_in_new,
+                              label: 'Open',
+                              onPressed: () {
+                                context
+                                    .read<ArticleCubit>()
+                                    .openUrl(context, article.title);
+                              },
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ],
-    );
-  }
+          ],
+        );
+      });
 }

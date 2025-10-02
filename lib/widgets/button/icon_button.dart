b64ac5dc4767d808c5ef851320f3f4583ef341b5
bradleyoosterveen <34512356+bradleyoosterveen@users.noreply.github.com>
2025-10-02 19:10:50 +0200

    Refactor saved articles screen and icon button (#12)

--- lib/widgets/button/icon_button.dart
+++ lib/widgets/button/icon_button.dart
@@ -3,38 +3,53 @@ import 'package:flutter/material.dart';
 class WikWokIconButton extends StatelessWidget {
   const WikWokIconButton({
     required this.icon,
-    required this.label,
+    this.label,
     this.onPressed,
     super.key,
   });
 
   final IconData icon;
-  final String label;
+  final String? label;
   final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: onPressed,
-      child: Container(
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-        ),
-        width: 64,
-        height: 64,
-        padding: const EdgeInsets.all(4),
-        child: Column(
-          children: [
-            Icon(
-              icon,
-              color: Theme.of(context).iconTheme.color,
-              size: 24,
-            ),
-            Text(
-              label,
-            ),
-          ],
+    final label = this.label;
+
+    return Material(
+      color: Colors.transparent,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          width: 48,
+          height: 48,
+          padding: const EdgeInsets.all(4),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: Theme.of(context).iconTheme.color,
+                size: 24,
+              ),
+              if (label != null) ...{
+                Flexible(
+                  child: FittedBox(
+                    child: Text(
+                      label,
+                      style: const TextStyle().copyWith(
+                        color: Theme.of(context).iconTheme.color,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ),
+              },
+            ],
+          ),
         ),
       ),
     );

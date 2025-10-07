ec54a3d5bc61f14e723bcb97fd3acb72b0597857
bradleyoosterveen <34512356+bradleyoosterveen@users.noreply.github.com>
2025-10-07 17:48:59 +0200

    Refactor widgets (#43)

--- lib/widgets/circular_progress.dart
+++ lib/widgets/circular_progress.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 
-Widget wCircularProgress = Center(
-  child: FCircularProgress.loader(
-    style: (style) => style.copyWith(
-      iconStyle: style.iconStyle.copyWith(
-        size: 32,
-      ),
-    ),
-  ),
-);
+class WCircularProgress extends StatelessWidget {
+  const WCircularProgress({super.key});
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: FCircularProgress.loader(
+          style: (style) => style.copyWith(
+            iconStyle: style.iconStyle.copyWith(
+              size: 32,
+            ),
+          ),
+        ),
+      );
+}

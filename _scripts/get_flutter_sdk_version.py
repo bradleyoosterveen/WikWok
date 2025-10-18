3513559def17ed4cd7279726fdd18bb5dddd002b
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2025-10-18 15:41:07 +0200

    Changes

--- _scripts/get_flutter_sdk_version.py
+++ _scripts/get_flutter_sdk_version.py
@@ -1,11 +1,10 @@
 import json
 import os
 import uuid
-import set_github_output_value
 
 with open('.fvmrc', 'r') as file:
     data = json.load(file)
 
 version = data['flutter']
 
-set_github_output_value.set_output('VERSION', version)
+print(version)
--- _scripts/set_github_output_value.py
+++
@@ -1,6 +0,0 @@
-import os
-import uuid
-
-def set_output(name, value):
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        fh.write(f'{name}={value}\n')

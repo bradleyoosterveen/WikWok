a2c4be03197b67e7d5a3cd4d4df5839a5b802ef5
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2025-10-18 15:32:36 +0200

    Changes

--- _scripts/set_github_output_value.py
+++ _scripts/set_github_output_value.py
@@ -1,13 +1,6 @@
 import os
 import uuid
 
-def set_multiline_output(name, value):
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        delimiter = uuid.uuid1()
-        print(f'{name}<<{delimiter}', file=fh)
-        print(value, file=fh)
-        print(delimiter, file=fh)
-
 def set_output(name, value):
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        print(f'{name}={value}', file=fh)
+        fh.write(f'{name}={value}\n')
